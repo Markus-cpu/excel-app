dfba377c2bb518fc2d4acce9b2d01fa3466dc29d
Markus-cpu <markbass82gmail.com>
2021-01-18 22:19:19 +0300

    initial framework

---
+++ src/components/toolbar/ToolBar.js
@@ -0,0 +1,8 @@
+import { ExcelComponent } from '../../core/ExcelComponent'
+
+export class ToolBar extends ExcelComponent {
+    toHTML() {
+        //return super.toHTML()
+        return '<h1>Formula</h1>'
+    }
+}
