be3d199bd21ca3db7d57d5532b3296d65c349315
Marat <markbass82@gmail.com>
2021-01-29 16:39:42 +0300

    table.js line changed 26, 27

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,14 +16,15 @@ export class Table extends ExcelComponent {
     onMousedown(event) {
         if (event.target.dataset.resize) {
             const $resizer = $(event.target)
-            console.log($resizer)
             // parent is Dom element
             const $parent = $resizer.closest('[data-type = "resize"]')
             const coords = $parent.getCoords()
 
             document.onmousemove = e => {
+                console.log(e)
                 const deltaX = e.pageX - coords.right
-                $parent.$el.style.width = (coords.width + deltaX) + 'px'
+                const value = coords.width + deltaX
+                $parent.$el.style.width = value + 'px'
                 console.log(deltaX)
             }
 
